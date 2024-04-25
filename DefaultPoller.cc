a63c6f962b33394063c0115ee55c083a177f8db3
GreatAlex <wangkangru@wangkangrudeMacBook-Air.local>
2024-04-25 15:53:45 +0800

    first commit

---
+++ DefaultPoller.cc
@@ -0,0 +1,13 @@
+#include "EPollPoller.h"
+#include "Poller.h"
+
+#include <stdlib.h>
+
+Poller *Poller::newDefaultPoller(EventLoop *loop)
+{
+    if (::getenv("MUDUO_USE_POLL")) {
+        return nullptr;
+    } else {
+        return new EPollPoller(loop);
+    }
+}
